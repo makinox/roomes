9b7524c58ca80c456109ed3fb727e2e3755cde8e
Jesus Bossa <x@jesusbossa.dev>
2021-10-06 20:14:46 -0500

    chore: navbar implemented

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
+  import './css/colors.css';
+  import '@makinox/makinox-ui/dist/index.css';
+  import Navbar from './components/navbar/navbar.svelte';
   export let name = 'world';
 </script>
+
+<Navbar />
 
 <h1>Hello {name}!</h1>
 
---
+++ src/components/navbar/navbar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { FluidContainer } from '@makinox/makinox-ui';
+  console.log('navbar');
+</script>
+
+<header class={FluidContainer()}>
+  <nav class="flex justify-between">
+    <div>
+      <span>Roomes</span>
+    </div>
+
+    <div>
+      <span>Ho</span>
+      <span>lin</span>
+      <span>back</span>
+    </div>
+  </nav>
+</header>
+
+<style>
+  nav {
+    margin-top: 10px;
+    border: 1px solid #000;
+  }
+</style>
