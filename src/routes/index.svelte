71ac5fdbb0fb8aeb24dbfb1fd83d45aa79594d8d
Jesus Bossa <x@jesusbossa.dev>
2021-10-12 22:07:01 -0500

    chore: trying to add sapper

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	
+</script>
+
+<svelte:head>
+	<title>Hola sapo</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<figure>
+	
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
